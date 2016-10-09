a8a44d8ecff5f692be968205cf2c520643344b5d
romain <rco@deezer.com>
2016-10-09 11:00:21 +0200

    Exception: Add no data exception

---
+++ src/Database/ExceptionNoData.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Copyright (c) 2010-2016 Romain Cottard
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eureka\Component\Database;
+
+/**
+ * Class to handle exception
+ *
+ * @author Romain Cottard
+ */
+class ExceptionNoData extends \RuntimeException
+{
+}
